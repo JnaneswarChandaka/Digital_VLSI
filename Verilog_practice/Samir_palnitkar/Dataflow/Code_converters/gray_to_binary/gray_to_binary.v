803ed4db7763b58f24c9e7fbcc22bee183f3f308
Jnaneswar <chandakajnaneswar@gmail.com>
2026-08-14 17:35:18 +0530

    Create gray_to_binary.v

---
+++ Verilog_practice/Samir_palnitkar/Dataflow/Code_converters/gray_to_binary/gray_to_binary.v
@@ -0,0 +1,11 @@
+// gray to binary code conversion
+
+
+module gray_to_bin(G, B);
+input [3:0] G;
+output [3:0]B;
+assign B[3] = G[3];
+assign B[2] = G[2] ^ B[3];
+assign B[1] = G[1] ^ B[2];
+assign B[0] = G[0] ^ B[1];
+endmodule
