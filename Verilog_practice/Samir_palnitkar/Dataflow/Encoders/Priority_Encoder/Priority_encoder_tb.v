55acad3ec56a2e8cd9a318301d433b980945d2af
Jnaneswar <chandakajnaneswar@gmail.com>
2026-08-23 21:25:22 +0530

    Create Priority_encoder_tb.v

---
+++ Verilog_practice/Samir_palnitkar/Dataflow/Encoders/Priority_Encoder/Priority_encoder_tb.v
@@ -0,0 +1,28 @@
+
+
+module priority_encoder_tb();
+reg [7:0]D;
+wire A,B,C;
+priority_encoder dut(D, A,B,C);
+initial begin
+$display("D         A   B   C");
+$monitor("%d        %d  %d   %d",D,A,B,C);
+//    D = 8'b10000000;
+//#10 D = 8'bx1000000;
+//#10 D = 8'bxx100000;
+//#10 D = 8'bxxx10000;
+//#10 D = 8'bxxxx1000;
+//#10 D = 8'bxxxxx100;
+//#10 D = 8'bxxxxxx10;
+//#10 D = 8'bxxxxxxx1;
+    D = 8'b10000000;
+#10 D = 8'b11000000;
+#10 D = 8'b10100000;
+#10 D = 8'b10110000;
+#10 D = 8'b01101000;
+#10 D = 8'b00100100;
+#10 D = 8'b10010010;
+#10 D = 8'b01110101;
+#10 $finish;
+end
+endmodule
