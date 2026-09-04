567f406f2e59d26b4414fa8363ddb1ab38867b93
Jnaneswar <chandakajnaneswar@gmail.com>
2026-09-04 23:55:02 +0530

    Fix demux assignments for correct output lines

--- Verilog_practice/Samir_palnitkar/Dataflow/Demux/Demux_1_to_8/demux_1_to_8.v
+++ Verilog_practice/Samir_palnitkar/Dataflow/Demux/Demux_1_to_8/demux_1_to_8.v
@@ -4,11 +4,11 @@ input A;
 input [2:0]S;
 output [7:0]Y;
 assign Y[0] = ~S[2] & ~S[1] & ~S[0] & A;
-assign Y[0] = ~S[2] & ~S[1] &  S[0] & A;
-assign Y[0] = ~S[2] &  S[1] & ~S[0] & A;
-assign Y[0] = ~S[2] &  S[1] &  S[0] & A;
-assign Y[0] =  S[2] & ~S[1] & ~S[0] & A;
-assign Y[0] =  S[2] & ~S[1] &  S[0] & A;
-assign Y[0] =  S[2] &  S[1] & ~S[0] & A;
-assign Y[0] =  S[2] &  S[1] &  S[0] & A;
+assign Y[1] = ~S[2] & ~S[1] &  S[0] & A;
+assign Y[2] = ~S[2] &  S[1] & ~S[0] & A;
+assign Y[3] = ~S[2] &  S[1] &  S[0] & A;
+assign Y[4] =  S[2] & ~S[1] & ~S[0] & A;
+assign Y[5] =  S[2] & ~S[1] &  S[0] & A;
+assign Y[6] =  S[2] &  S[1] & ~S[0] & A;
+assign Y[7] =  S[2] &  S[1] &  S[0] & A;
 endmodule
