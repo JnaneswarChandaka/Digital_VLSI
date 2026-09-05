59e88265d78ad7a39d49e4e633ba7a5115481328
Jnaneswar <chandakajnaneswar@gmail.com>
2026-09-05 11:59:04 +0530

    Add 1-to-16 Demultiplexer module in Verilog

---
+++ Verilog_practice/Samir_palnitkar/Dataflow/Demux/demux_1_to_16/demux_1_to_16.v
@@ -0,0 +1,21 @@
+module Demux_1_to_16(A,S,Y);
+input A;
+input [3:0]S;
+output [15:0]Y;
+assign Y[0] = ~S[3] & ~S[2] & ~S[1] & ~S[0] & A;
+assign Y[1] = ~S[3] & ~S[2] & ~S[1] &  S[0] & A;
+assign Y[2] = ~S[3] & ~S[2] &  S[1] & ~S[0] & A;
+assign Y[3] = ~S[3] & ~S[2] &  S[1] &  S[0] & A;
+assign Y[4] = ~S[3] &  S[2] & ~S[1] & ~S[0] & A;
+assign Y[5] = ~S[3] &  S[2] & ~S[1] &  S[0] & A;
+assign Y[6] = ~S[3] &  S[2] &  S[1] & ~S[0] & A;
+assign Y[7] = ~S[3] &  S[2] &  S[1] &  S[0] & A;
+assign Y[8] =  S[3] & ~S[2] & ~S[1] & ~S[0] & A;
+assign Y[9] =  S[3] & ~S[2] & ~S[1] &  S[0] & A;
+assign Y[10] = S[3] & ~S[2] &  S[1] & ~S[0] & A;
+assign Y[11] = S[3] & ~S[2] &  S[1] &  S[0] & A;
+assign Y[12] = S[3] &  S[2] & ~S[1] & ~S[0] & A;
+assign Y[13] = S[3] &  S[2] & ~S[1] &  S[0] & A;
+assign Y[14] = S[3] &  S[2] &  S[1] & ~S[0] & A;
+assign Y[15] = S[3] &  S[2] &  S[1] &  S[0] & A;
+endmodule
