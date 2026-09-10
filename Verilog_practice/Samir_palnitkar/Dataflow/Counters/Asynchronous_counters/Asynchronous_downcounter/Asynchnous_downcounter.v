d9e96592e40dea38c9943746ead6d41b7a61299e
Jnaneswar <chandakajnaneswar@gmail.com>
2026-09-10 22:36:52 +0530

    Update Asynchnous_downcounter.v

--- Verilog_practice/Samir_palnitkar/Dataflow/Counters/Asynchronous_counters/Asynchronous_downcounter/Asynchnous_downcounter.v
+++ Verilog_practice/Samir_palnitkar/Dataflow/Counters/Asynchronous_counters/Asynchronous_downcounter/Asynchnous_downcounter.v
@@ -1,1 +1,13 @@
 // Asynchronous Downcounter
+
+
+module downcounter_4_bit(clk, rst, cnt);
+input clk, rst;
+output reg[3:0]cnt;
+initial cnt = 4'b1111;
+always@(posedge clk)begin
+if(rst) cnt<= 4'b0000;
+else
+cnt <= cnt - 1'b1; 
+end
+endmodule
