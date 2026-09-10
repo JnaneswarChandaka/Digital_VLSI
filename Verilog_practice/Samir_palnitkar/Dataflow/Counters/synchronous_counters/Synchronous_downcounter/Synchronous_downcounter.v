fb87ca33cf557c72abde97ea7a5b132b01937263
Jnaneswar <chandakajnaneswar@gmail.com>
2026-09-10 23:33:23 +0530

    Update Synchronous_downcounter.v

--- Verilog_practice/Samir_palnitkar/Dataflow/Counters/synchronous_counters/Synchronous_downcounter/Synchronous_downcounter.v
+++ Verilog_practice/Samir_palnitkar/Dataflow/Counters/synchronous_counters/Synchronous_downcounter/Synchronous_downcounter.v
@@ -1,1 +1,14 @@
 // Synchronous Downcounter
+
+
+module Synchronous_downcounter(clk, rst, cnt);
+input clk, rst;
+output reg [3:0]cnt;
+always@(posedge clk or negedge rst) begin
+if(!rst)
+cnt <= 4'b1111;
+else
+cnt <= cnt - 1'b1;
+
+end
+endmodule
