aaad97146443c12e0cca24e10da007aeeccd8dee
Jnaneswar <chandakajnaneswar@gmail.com>
2026-09-10 22:11:38 +0530

    Add testbench for 4-bit asynchronous upcounter

---
+++ Verilog_practice/Samir_palnitkar/Dataflow/Counters/Asynchronous_counters/Asynchnous_upcounter/Asynchronous_upcounter_tb.v
@@ -0,0 +1,24 @@
+// Asynchronous upcounter testbench
+
+
+module upcounter_4_bit_tb();
+reg clk, rst;
+wire [3:0] cnt;
+
+upcounter_4_bit dut(.clk(clk), .rst(rst), .cnt(cnt));
+
+always #10 clk = ~clk;
+initial begin
+$monitor("clk = %0t, rst = %b, cnt = %b", $time, rst, cnt);
+clk = 0;
+rst = 1;
+#20 rst = 0;
+
+#250 rst = 1;
+
+#20 rst = 0;
+
+#600 $finish;
+end
+
+endmodule
