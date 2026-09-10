419ed7a1cae5269eaf025a0a4546c7b2daaa8dd2
Jnaneswar <chandakajnaneswar@gmail.com>
2026-09-10 23:22:36 +0530

    Update Synchronous_upcounter.v

--- Verilog_practice/Samir_palnitkar/Dataflow/Counters/synchronous_counters/Synchronous_upcounter/Synchronous_upcounter.v
+++ Verilog_practice/Samir_palnitkar/Dataflow/Counters/synchronous_counters/Synchronous_upcounter/Synchronous_upcounter.v
@@ -1,1 +1,16 @@
 // Synchronous 4 bit Upcounter
+
+
+
+
+module Synchronous_upcounter(clk, rst, cnt);
+input clk, rst;
+output reg [3:0]cnt;
+always@(posedge clk or negedge rst) begin
+if(!rst)
+cnt <= 4'b0000;
+else
+cnt <= cnt + 1'b1;
+
+end
+endmodule
