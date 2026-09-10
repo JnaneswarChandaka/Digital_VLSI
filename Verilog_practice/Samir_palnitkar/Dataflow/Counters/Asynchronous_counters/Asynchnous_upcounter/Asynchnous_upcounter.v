1b5f43786810c143e2f991e2334dae39123f8683
Jnaneswar <chandakajnaneswar@gmail.com>
2026-09-10 22:10:31 +0530

    Update Asynchnous_upcounter.v

--- Verilog_practice/Samir_palnitkar/Dataflow/Counters/Asynchronous_counters/Asynchnous_upcounter/Asynchnous_upcounter.v
+++ Verilog_practice/Samir_palnitkar/Dataflow/Counters/Asynchronous_counters/Asynchnous_upcounter/Asynchnous_upcounter.v
@@ -1,1 +1,13 @@
 // Asynchronous upcounter
+
+
+module upcounter_4_bit(clk, rst, cnt);
+input clk, rst;
+output reg[3:0]cnt;
+initial cnt = 4'b0000;
+always@(posedge clk)begin
+if(rst) cnt<= 4'b0000;
+else
+cnt <= cnt + 1'b1; 
+end
+endmodule
